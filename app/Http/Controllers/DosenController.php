b33710159556d4f37474c9bb67de6312f1455a57
AhmadNabil <147696638+ahmadnabyl@users.noreply.github.com>
2023-11-16 15:18:40 +0700

    pwebweek12

--- app/Http/Controllers/DosenController.php
+++ app/Http/Controllers/DosenController.php
@@ -14,11 +14,23 @@ public function index(){
     return "<h1> Hasil Perkalian = " . $c . "</h1>";
 
 }
-public function index(){
+public function biodata(){
     $nama = "Ahmad Nabil";
     $alamat = "Surabaya";
     $umur = 19;
     return view('biodata',['nama' => $nama, 'alamat' => $alamat, 'umur' => $umur]);
+}
+public function showtime($jam){
+    return "<h1>$jam</h1>";
+}
+public function formulir(){
+    return view('formulir');
+}
+public function proses(Request $request){
+    $nama = $request->input('nama');
+     $alamat = $request->input('alamat');
+     $ipk= $request->input('ipk');
+    return "Nama : ".$nama.", Alamat : ".$alamat . ", IPK : ". $ipk . ", isi asli :" . $request;
 }
 }
 
--- routes/web.php
+++ routes/web.php
@@ -26,7 +26,7 @@ Route::get('halo2', function () {
     return "<h1>Halo apa kabar</h1>" ;
 });
 
-Route::get('blog', function () {
+Route::get('blog1', function () {
 	return view('blog');
 });
 
@@ -71,5 +71,20 @@ Route::get('/latihan7', function () {
 });
 
 Route::get('perkalian', 'App\Http\Controllers\DosenController@index');
+Route::get('biodata', 'App\Http\Controllers\DosenController@biodata');
+Route::get('showjam/{jam}', 'App\Http\Controllers\DosenController@showtime');
+Route::get('formulir', 'App\Http\Controllers\DosenController@formulir');
+Route::post('/formulir/proses', 'App\Http\Controllers\DosenController@proses');
 
 
+Route::get('/blog', function () {
+    return view('home');
+});
+
+Route::get('/blog/tentang', function () {
+    return view('tentang');
+});
+
+Route::get('/blog/kontak', function () {
+    return view('kontak');
+});
