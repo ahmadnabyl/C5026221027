cfe8fe34958b19d970e250f5608736d28e16841c
AhmadNabil <147696638+ahmadnabyl@users.noreply.github.com>
2023-11-30 14:57:18 +0700

    Pertemuan 30 Nop 2023

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-  <title> @yield('judulhalaman') </title>
+  <title>@yield('judulhalaman')</title>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css">
@@ -13,13 +13,13 @@
 
 <div class="container">
     <header>
-        <h2>Blog Mirnabil</h2>
+        <h2>Blog Afandi Sinyo</h2>
         <nav>
-                <a href="/blog">HOME</a>
-                |
-                <a href="/blog/tentang">TENTANG</a>
-                |
-                <a href="/blog/kontak">KONTAK</a>
+            <a href="/blog">HOME</a>
+            |
+            <a href="/blog/tentang">TENTANG</a>
+            |
+            <a href="/blog/kontak">KONTAK</a>
         </nav>
     </header>
     <hr/>
@@ -27,12 +27,10 @@
     @yield('judul')
     <br/>
     @yield('konten')
-    <br/>
-    <br/>
-    <hr/>
     <footer>
-            <p>&copy; <a href="https://www.malasngoding.com">www.malasngoding.com</a>. 2018 - 2019</p>
+        <p>&copy; <a href="https://www.malasngoding.com">www.malasngoding.com</a>. 2018 - 2019</p>
     </footer>
 </div>
+
 </body>
 </html>
