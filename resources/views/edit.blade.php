bcb48e798276728ba1fe8b7dfc53628a75f943d3
AhmadNabil <147696638+ahmadnabyl@users.noreply.github.com>
2023-12-06 17:52:34 +0700

    Tugas pertemuan tanggal 30 Nop

--- resources/views/edit.blade.php
+++ resources/views/edit.blade.php
@@ -1,48 +1,44 @@
 @extends('master2')
-@section('judulhalaman','Data Pegawai')
+@section('judulhalaman', 'Data Pegawai')
 
 @section('konten')
 
-	<h2><a href="https://www.malasngoding.com">www.malasngoding.com</a></h2>
-	<h3>Edit Pegawai</h3>
+    <h2><a href="https://www.malasngoding.com">www.malasngoding.com</a></h2>
+    <h3>Edit Pegawai</h3>
 
-	<a href="/pegawai" class="btn btn-success"> <- Kembali</a>
+    <a href="/pegawai" class="btn btn-success"> <- Kembali</a>
 
-	<br/>
-	<br/>
-
+    <br/>
+    <br/>
 	@foreach($pegawai as $p)
-	<form action="/pegawai/update" method="post">
+	<form action="/pegawai/update" method="post" class="form-horizontal">
 		{{ csrf_field() }}
         <div class="form-group row">
-            <label for="nama" class="col-xs-3 col-form-label mr-2">Nama</label>
-            <div class="col-xs-9">
-            <input type="text" class="form-control" id="nama" name="nama">
+            <label for="nama" class="col-xs-4 col-form-label mr-2">Nama</label>
+            <input type="hidden" name="id" value="{{ $p->pegawai_id }}">
+            <div class="col-xs-8">
+                <input type="text" required="required" class="form-control" id="nama" name="nama" value="{{ $p->pegawai_nama }}">
             </div>
         </div>
         <div class="form-group row">
             <label for="jabatan" class="col-xs-4 col-form-label mr-2">Jabatan</label>
             <div class="col-xs-8">
-            <input type="text" class="form-control" id="jabatan" name="jabatan">
+                <input type="text" required="required" class="form-control" id="jabatan" name="jabatan" value="{{$p->pegawai_jabatan}}">
             </div>
         </div>
         <div class="form-group row">
             <label for="umur" class="col-xs-4 col-form-label mr-2">Umur</label>
             <div class="col-xs-8">
-            <input type="number" class="form-control" id="umur" name="umur">
+                <input type="text" required="required" class="form-control" id="umur" name="umur" value="{{$p->pegawai_umur}}">
             </div>
         </div>
         <div class="form-group row">
             <label for="alamat" class="col-xs-4 col-form-label mr-2">Alamat</label>
             <div class="col-xs-8">
-            <textarea name="alamat" class="form-control" id="alamat"></textarea>
+                <textarea required="required" id="alamat" class="form-control" name="alamat">{{ $p->pegawai_alamat }}</textarea>
             </div>
         </div>
-		<input type="submit" value="Simpan Data" class="btn btn-primary">
+        <input type="submit" value="Simpan Data" class="btn btn-primary">
 	</form>
 	@endforeach
-
-</body>
-</html>
-
 @endsection
